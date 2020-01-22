64ac0718af60fa9195d54dbd279ca5fb5318c7f5
EL BAHLOUL Abdelali <bahloul97abdel@gmail.com>
2020-01-22 12:49:01 +0100

    test

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -22,5 +23,8 @@ namespace generateClassFromXSD
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+           WebHost.CreateDefaultBuilder(args)
+           .UseStartup<Startup>().UseUrls("http://127.0.0.1:2222");
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,7 @@ namespace generateClassFromXSD
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,7 +40,7 @@ namespace generateClassFromXSD
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
@@ -51,7 +52,9 @@ namespace generateClassFromXSD
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
+            
         }
     }
 }
