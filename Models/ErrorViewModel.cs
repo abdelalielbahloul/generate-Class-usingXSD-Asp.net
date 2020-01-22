11890a87f296c738d7b7d77283a012ce1c590d97
Abdelali EL BAHLOUL <bahloul97abdel@gmail.com>
2020-01-22 10:27:03 +0100

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace generateClassFromXSD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
