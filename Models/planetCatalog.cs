c00679b7098ef9f3ea4dd148c56ecf3ada16a6d9
EL BAHLOUL Abdelali <bahloul97abdel@gmail.com>
2020-01-22 11:52:02 +0100

    add a test.xml file

---
+++ Models/planetCatalog.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace generateClassFromXSD.Models
+{
+    public class planetCatalog
+    {
+    }
+}
